2cc16a3a3d4ab7250fed29008b811948d4350597
MBD <jason994842@gmail.com>
2026-07-15 20:44:44 +0800

    做兩個VUE

---
+++ src/app/StatsPanel.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useShortcuts } from '@/composables/useShortcuts';
+
+useShortcuts();
+</script>
+
+<template>
+    <UApp>
+        <RouterView />
+    </UApp>
+</template>
